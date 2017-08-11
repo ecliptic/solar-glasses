e0e6fd4f40bfbed68881c26c89193a6bd0a12801
mssngr <gabriel@ecliptic.io>
2017-08-11 15:25:06 -0600

    stuff

---
+++ src/components/Card.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card">
+    <img class="image" src="image" />
+    <div  />
+    <Tile v-for="tile in row3" />
+  </div>
+</template>
+
+<script>
+import Tile from './Tile'
+export default {
+  name: 'Map',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card {
+  position: absolute;
+  width: 66%;
+  height: 90%;
+  background-color: white;
+}
+
+.image {
+
+}
+</style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -16,4 +16,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+body {
+  width: 100vw;
+  height: 100vh;
+}
 </style>
