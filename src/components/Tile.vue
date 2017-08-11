0b5237bcf9596514d6d2f4821eb37b7145a09f1b
mssngr <gabriel@ecliptic.io>
2017-08-11 15:16:37 -0600

    Added Tile component.

---
+++ src/components/Tile.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="tile">
+    <div class="icon" v-if="type === 'grass'" />
+    <div v-else src="../../assets/images/icons/tree.png">
+      <img class="icon" src="../../assets/images/icons/tree.png" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tile',
+  props: [
+    'type',
+  ],
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.tile {
+  width: 30vh;
+  height: 30vh;
+  background-color: green;
+}
+
+.icon {
+  width: 100%;
+  height: 100%;
+}
+</style>
